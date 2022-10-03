f7336d49a1bae2900faa0789cd6a57ec4e5efbe7
Lannah Saldana <93993356+lhbsaldana@users.noreply.github.com>
2022-10-03 17:53:20 +0800

    Final update for server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 - Setup the server and create a new Express app
 - Setup the View Engine of the Express to be EJS
 - Allow the new application to open the url '/' to open index.ejs and /other to open the other.ejs file
-
+<img class="img-fluid" src= <%= "http://openweathermap.org/img/w/"+(weatherAPI['list'][0].weather[0].icon)+ ".png"; %>  width="800px" style="height: 9.5rem"  alt="Responsive image" />
 */ 
 const express = require ('express'); 
 const app = express(); 
@@ -39,18 +39,18 @@ fetchUrl("https://api.openweathermap.org/data/2.5/forecast?q=Davao&units=metric&
 /*routing 
 '/' redirects end user to index.ejs page  */ 
 
-var city = "Davao City" 
+var city = "Cebu" 
 
 app.get('/',function(req,res){ 
     weather.find({search: city, degreeType: 'C'}, function(err, result) {
         if(err) console.log(err);
         var  dataJS = result; // object data 
-        console.log(dataJS)
         var fromWeatherJS = { 
             weatherJS : dataJS}//convert to JSON object
         
-        fetchUrl("https://api.openweathermap.org/data/2.5/forecast?q=Davao&units=metric&appid=4a13c29e47abc7587b529fde28e8d6ba",function(error,meta,body){
-            dataAPI= JSON.parse(body); 
+        fetchUrl("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=4a13c29e47abc7587b529fde28e8d6ba",function(error,meta,body){
+            var dataAPI= JSON.parse(body); 
+            console.log(dataAPI)
             var fromAPI = { 
                 weatherAPI : dataAPI }//convert to JSON object
 
@@ -61,6 +61,20 @@ app.get('/',function(req,res){
 
 // '/other' redirects end user to other.ejs page 
 app.get('/other',function(req,res){
-    res.render('other')
+    weather.find({search: city, degreeType: 'C'}, function(err, result) {
+        if(err) console.log(err);
+        var  dataJS = result; // object data 
+        console.log(dataJS)
+        var fromWeatherJS = { 
+            weatherJS : dataJS}//convert to JSON object
+        
+        fetchUrl("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=4a13c29e47abc7587b529fde28e8d6ba",function(error,meta,body){
+            dataAPI= JSON.parse(body); 
+            var fromAPI = { 
+                weatherAPI : dataAPI }//convert to JSON object
+
+    res.render('other',{weatherJS:dataJS, weatherAPI:dataAPI})
+        });
+    });
 }); 
 
