f10475d1cb591b7b413df4be93d4bff0dedf3e69
lhbsaldana <lannahhareille@gmail.com>
2022-10-20 21:51:59 +0800

    add first commit

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <body>
-    <h1>About Us</h1>
-    <h2>We merge tradition with technology</h2>
-
-  </body>
-</html>
 
 
-/**like a server.js file 
+
+/**like a server.js file **/ 
 
 var express = require('express'); 
 var app = express(); 
 app.use(express.static('assets')); //to use assets folder
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 2020; 
 app.listen(port,); 
 app.set('view engine','ejs'); 
 
-app.get('/',asyncfunction(req,res){
-    let data = {
-        url:req.url, 
-    }
-    res.render('pages/index',data);
-}); **/ 
+app.get('/',function(req,res){
+    res.render('pages/index');
+}); 
