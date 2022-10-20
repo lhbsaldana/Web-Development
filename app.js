83b6f60edbd7a4e3c2cc4f0b74f0d5711453e9e3
lhbsaldana <lannahhareille@gmail.com>
2022-10-20 22:02:48 +0800

    import necessary packages

--- app.js
+++ app.js
@@ -5,12 +5,14 @@
 
 var express = require('express'); 
 var app = express(); 
-app.use(express.static('assets')); //to use assets folder
+const path = require('path'); 
+app.set('pages',path.join(__dirname,'/pages'));
+app.use(express.static('templates'));
 
 const port = process.env.PORT || 2020; 
 app.listen(port,); 
-app.set('view engine','ejs'); 
+
 
 app.get('/',function(req,res){
-    res.render('pages/index');
+    res.render('index.ejs');
 }); 
