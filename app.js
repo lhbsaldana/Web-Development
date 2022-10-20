73384874a68b2127b5adac03877789252855817d
lhbsaldana <lannahhareille@gmail.com>
2022-10-20 20:57:20 +0800

    delete unwanted files

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-//like a server.js file 
+<!DOCTYPE html>
+<html lang="en">
+
+  <body>
+    <h1>About Us</h1>
+    <h2>We merge tradition with technology</h2>
+
+  </body>
+</html>
+
+
+/**like a server.js file 
 
 var express = require('express'); 
 var app = express(); 
@@ -13,4 +24,4 @@ app.get('/',asyncfunction(req,res){
         url:req.url, 
     }
     res.render('pages/index',data);
-}); 
+}); **/ 
