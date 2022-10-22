a706695e874887a4063f3823057c33defc5741f5
lhbsaldana <lannahhareille@gmail.com>
2022-10-22 11:54:45 +0800

    first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 app.set('pages',path.join(__dirname,'/pages'));
 app.use(express.static('templates'));
 
-const port = process.env.PORT || 8000; 
+const port = process.env.PORT || 5500; 
 app.listen(port,); 
 
 
