25035a228aca05fab1c6d898329a1adc9a1a009a
lhbsaldana <lannahhareille@gmail.com>
2022-10-22 13:01:18 +0800

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-
-
-
 /**like a server.js file **/ 
 
 var express = require('express'); 
 var app = express(); 
 const path = require('path'); 
 app.set('pages',path.join(__dirname,'/pages'));
-app.use(express.static('templates'));
+app.use(express.static('Green'));
 
 const port = process.env.PORT || 5500; 
-app.listen(port,); 
+app.listen(port); 
 
+
+app.use((req,res,next) => {
+    console.log("Request Mode"); 
+    console.log(`Host: ${req.hostname}`);
+    console.log(`Path: ${req.path}`);
+    console.log(`Method: ${req.method}`);
+    next();
+}); 
 
 app.get('/',function(req,res){
     res.render('index.ejs');
